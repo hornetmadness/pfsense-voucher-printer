3435f095bc520e80452c15e310802571af87764a
Erik Mathis <erik.mathis@salesloft.com>
2020-02-21 14:16:30 -0500

    changed tcpport back to 5000

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ db_string = "sqlite:///database/vouchers.sqlite3"
 db_debug=False
 debug=False
 
-tcpPort = os.environ.get("TCPPORT", "4040")
+tcpPort = os.environ.get("TCPPORT", "5000")
 ifaceAddress = os.environ.get("IFADDR", "127.0.0.1")
 
 force_ssl = True
